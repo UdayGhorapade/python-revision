0845f55bfddd81d99d45254f0cd0e225258cd701
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-09-12 12:55:59 +0530

    Add files via upload

---
+++ python functions .py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # python functions 
+
+# In[20]:
+
+
+#print
+firstname ='uday'
+lastname='ghorapade'
+print(firstname)
+print(lastname)
+print(firstname  +  '-' ,lastname )
+
+
+# In[22]:
+
+
+fruit='Apple'
+print('i have an ' + fruit)
+
+
+# In[24]:
+
+
+#print format 
+fruit1='apple'
+fruit2='banana'
+print('i have 2 fruits, one is {} other one is {}'.format(fruit1,fruit2))
+
+
+# In[25]:
+
+
+print('i have 2 fruits, one is {1} other one is {0}'.format(fruit1,fruit2))
+
+
+# In[29]:
+
+
+#2)Input 
+name=input('enter your name:')
+print(name)
+
+
+# In[30]:
+
+
+num1=input('enter 1st number:')
+num2= input('enter 2nd numer:')
+print(num1+num2)
+
+
+# In[31]:
+
+
+num1=input('enter 1st number:')
+num2= input('enter 2nd numer:')
+num3=num1+num2
+print(num3)
+
+
+# In[ ]:
+
+
+
+
