2402f91fcad7a48bdc9cd771f8ff566089b79ad6
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-09-12 19:31:47 +0530

    Add files via upload

---
+++ indexing & slicing .py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Indexing 
+
+# In[1]:
+
+
+a='hello my name is uday'
+print(a[0])
+
+
+# In[2]:
+
+
+print(a[6])
+
+
+# In[3]:
+
+
+print(a[-3])
+
+
+# # slicing
+
+# In[7]:
+
+
+a[0:5:1]
+
+
+# In[11]:
+
+
+a[17:21:1]
+
+
+# In[12]:
+
+
+a[0:-5:1]
+
+
+# In[ ]:
+
+
+
+
