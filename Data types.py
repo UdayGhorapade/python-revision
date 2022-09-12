abd02b8734d44266220dcd53b40917e51222db25
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-09-12 17:21:19 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data types 
+
+# In[1]:
+
+
+a=10
+type(a)
+
+
+# In[4]:
+
+
+b=10.5
+print(type(b))
+c='uday'
+print(type(c))
+
+
+# In[5]:
+
+
+len(c)
+
+
+# In[6]:
+
+
+d=[1,2,3,4,5]
+type(d)
+
+
+# In[8]:
+
+
+e={1,2,3,4,'uday'}
+type(e)
+
+
+# In[9]:
+
+
+f={'a':'b'}
+type(f)
+
+
+# In[10]:
+
+
+g=(1,2,3,4,5,6)
+type(g)
+
+
+# In[11]:
+
+
+h=True
+type(h)
+
+
+# In[12]:
+
+
+i=False
+type(i)
+
+
+# In[ ]:
+
+
+
+
