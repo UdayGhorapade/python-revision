76bc95ae79bdc7d6e6c7491460984d2246dd1190
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-09-12 19:32:47 +0530

    Add files via upload

---
+++ other functions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#some other functions 
+a='uday'
+print(a.upper())
+
+
+# In[6]:
+
+
+a='UDAY'
+print(a.lower())
+
+
+# In[7]:
+
+
+print(a.startswith('b'))
+
+
+# In[9]:
+
+
+print(a.endswith('k'))
+
+
+# In[10]:
+
+
+c='hello world'
+print(c.capitalize())
+
+
+# In[ ]:
+
+
+
+
