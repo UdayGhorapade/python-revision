b770a5a65e8da4e3c67e31f314d22e4be5cdd181
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-09-16 20:28:09 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # tuple
+
+# In[2]:
+
+
+tup1=(1,2,3,4,5)
+tup1[0]
+
+
+# In[3]:
+
+
+#convert to a list
+list1=list(tup1)
+list1
+
+
+# In[5]:
+
+
+#modify the values 
+list1[0]=100
+list1
+
+
+# In[6]:
+
+
+tup1=tuple(list1)
+tup1
+
+
+# In[7]:
+
+
+#joining the tuples 
+tup1=(1,2,3,4,5)
+tup2=('uday','sarita')
+tup3=tup1+tup2
+tup3
+
+
+# In[ ]:
+
+
+
+
