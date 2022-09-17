a283c346a19c95eeeee5c1d8fbbc494624a602ef
UdayGhorapade <95113723+UdayGhorapade@users.noreply.github.com>
2022-09-17 20:45:05 +0530

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Set
+
+# In[2]:
+
+
+a={1,2,3,4,5,'uday'}
+b={6,6,7,7,7,'ghorapade'}
+set3=a.union(b)
+set3
+
+
+# In[11]:
+
+
+a={1,2,3,4,5,'uday'}
+a
+
+
+# In[12]:
+
+
+b={6,6,7,7,7,'ghorapade'}
+b.remove('ghorapade')
+b
+
+
+# In[14]:
+
+
+b.clear()
+b
+
+
+# In[ ]:
+
+
+
+
